c41de3141ead5557a22c3beb2f5a4246c64f4058
= <adedejifavour6@gmail.com>
2022-09-28 21:30:49 +0100

    check input validation before creating a new task

--- src/modules/add_task.js
+++ src/modules/add_task.js
@@ -3,6 +3,10 @@ const input = document.querySelector('.input');
 const addToLists = (tasks) => {
   input.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
+      if (!input.value) {
+        alert('Please enter a task');
+        return;
+      }
       const eachTask = {
         description: input.value,
         completed: false,
