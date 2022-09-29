093ea6d34dbbb800b8a7d19991b929898ffa6d83
= <adedejifavour6@gmail.com>
2022-09-29 12:18:17 +0100

    Modify edit functionality

--- src/modules/edit_task.js
+++ src/modules/edit_task.js
@@ -2,11 +2,42 @@ const editingTask = () => {
   const element = document.querySelectorAll('.text');
   element.forEach((ele) => {
     ele.addEventListener('click', () => {
+      const tasks = JSON.parse(localStorage.getItem('TODO'));
       ele.contentEditable = true;
-      //   ele.style.backgroundColor = 'lightyellow';
       ele.parentElement.parentElement.lastElementChild.previousElementSibling.classList.add('hidden');
       ele.parentElement.parentElement.lastElementChild.classList.remove('hidden');
+
+      ele.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+          if (!ele.value) {
+            alert('Please enter a task');
+            return;
+          }
+
+          tasks.forEach((task) => {
+            if (task.index === +ele.dataset.id) {
+              task.description = ele.value;
+            }
+          });
+          localStorage.setItem('TODO', JSON.stringify(tasks));
+          window.location.reload();
+        }
+      });
+      // if (!ele.value) {
+      //   alert('Please enter a task')
+      // }
     });
+    // ele.addEventListener('change', () => {
+    //   console.log(ele.value);
+    //   tasks.forEach((task) => {
+    //     if (task.index === +ele.previousElementSibling.dataset.id) {
+    //       task.description = ele.textContent;
+    //       console.log(task.description);
+    //       console.log('hi');
+    //     }
+    //     localStorage.setItem('TODO', JSON.stringify(tasks));
+    //   });
+    // });
   });
 };
 
