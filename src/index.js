781bc188f48e1bc43db7ee99cb86cd88744acc89
= <adedejifavour6@gmail.com>
2022-09-29 13:30:33 +0100

    Add and import some functionality to the index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import './style.css';
 import editingTask from './modules/edit_task.js';
-import removeSingleTask from './modules/remove_task.js';
+import removeSingleTask, { removeCompletedTasks } from './modules/remove_task.js';
 import addToLists from './modules/add_task.js';
 import renderLists from './modules/display_task.js';
+import { checkboxClick, checked } from './modules/checked.js';
 
 renderLists();
 
+document.addEventListener('DOMContentLoaded', () => {
+  const tasks = JSON.parse(localStorage.getItem('TODO')) || [];
+  document.querySelectorAll('#checks').forEach((check) => {
+    checked(check, tasks);
+  });
+});
+
 addToLists();
+
+checkboxClick();
 
 editingTask();
 
 removeSingleTask();
+
+removeCompletedTasks();
