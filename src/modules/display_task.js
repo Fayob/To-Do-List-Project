0f553c2dfdd6d43c5bf134041a7db1e67f29107c
= <adedejifavour6@gmail.com>
2022-09-29 12:17:31 +0100

    Modify display functionality

--- src/modules/display_task.js
+++ src/modules/display_task.js
@@ -1,17 +1,18 @@
 const ul = document.querySelector('.to_do_lists');
 
-const renderLists = (tasks) => {
+const renderLists = () => {
+  const tasks = JSON.parse(localStorage.getItem('TODO')) || [];
   tasks.forEach((task) => {
     const li = document.createElement('li');
     li.classList.add('to_do_list');
     li.innerHTML = `
- <div class="container">
- <input type="checkbox" name="check" id="checks" data-id=${task.index} />
- <p class="text">${task.description} </p>
- </div>
- <i class="fa fa-ellipsis-v"></i>
- <i class="fa fa-trash hidden" ></i>
- `;
+   <div class="container">
+   <input type="checkbox" name="check" id="checks" data-id=${task.index} />
+   <input class="text" data-id=${task.index} value='${task.description}'  />
+   </div>
+   <i class="fa fa-ellipsis-v"></i>
+   <i class="fa fa-trash hidden" ></i>
+   `;
     ul.appendChild(li);
   });
 };
