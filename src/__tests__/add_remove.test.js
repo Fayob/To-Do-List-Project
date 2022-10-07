57188937507431cd44c85aa6c7836f9d966030f6
Okechukwu <okei.muokwugwo@outlook.com>
2022-10-07 14:45:45 +0100

    Add test for the add task function

--- src/__tests__/add_remove.test.js
+++ src/__tests__/add_remove.test.js
@@ -1,7 +1,13 @@
-// import addTask from '../modules/add-task.js';
-// import storedArray from '../modules/storage.js';
+import addTask from '../modules/add-task.js';
+import storedArray from '../modules/storage.js';
 // import removeTask from '../modules/remove-task.js';
 
 describe('test addTask and removeTask', () => {
-
+  test('test add task function', () => {
+    addTask('first task');
+    addTask('second task');
+    addTask('third task');
+    expect(storedArray[1].description).toBe('second task');
+    expect(storedArray.length).toEqual(3);
+  });
 });
