240daa19f888b2193fd7e5c79643dfd9daf1dd0b
= <adedejifavour6@gmail.com>
2022-10-07 16:08:50 +0100

    Add edit and clear completed testing file

---
+++ src/__tests__/edit_completed.test.js
@@ -0,0 +1,9 @@
+// import addTask from '../modules/add-task.js';
+// import storedArray from '../modules/storage.js';
+// import removeTask from '../modules/remove_task.js';
+// import editTask from '../modules/edit-task.js';
+// import clearCompletedTask from '../modules/clear-completed-task.js';
+
+describe('test addTask and removeTask', () => {
+
+});
